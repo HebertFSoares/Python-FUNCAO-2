dc3b31ff3f082f1102c925e0b630b4a4a98406f9
Hebert Soares <hebertsoaresof@gmail.com>
2023-02-16 11:58:47 -0300

    Finalização dos exercícios.

--- Funções/D03.py
+++ Funções/D03.py
@@ -1,4 +1,12 @@
-def fichar(nome = "<desconhecido>", gols = 0):
-    nome = str(input("Digite o nome do jogador: "))
-    gols = int(input("Digite a quantidade de gols: "))
-print(fichar)
+def ficha(jog = "<desconhecido>", gol = 0):
+    print(f"O jogador {jog} fez {gol} gol(s) no campeonato")
+nome = str(input("Digite o nome do jogador: "))
+gols = str(input("Digite a quantidade de gols: "))
+if gols.isnumeric():
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == "":
+    ficha(gol=gols)
+else:
+    ficha(nome,gols)
